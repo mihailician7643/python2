a0af683050143b94c2c457cf4a31723e4991499e
mihailician7643 <mihailician7643@gmail.com>
2022-12-11 00:10:03 +0200

    2nd comittment

--- test_perfect.py
+++ test_perfect.py
@@ -1,6 +1,16 @@
 from perfect import perfect
 
-def test_perfect():
+def test_perfect1():
     assert perfect(6) == 6
+
+def test_perfect2():
+    assert perfect(28) == 28
+
+def test_perfect3():
+    assert perfect(496) == 496
+    
+def test_perfect4():
+    assert perfect(8128) == 8128    
+    
     
     
